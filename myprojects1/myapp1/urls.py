64cb3a26a80d7c8531f47c134cc0a0141822d2e4
Satish Shivaji Satre <satish.satre@datamoulds.com>
2024-02-16 12:56:33 +0530

    content search changes

--- myprojects1/myapp1/urls.py
+++ myprojects1/myapp1/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', UserLogin.as_view(), name='user_login'),
     path("seed_database/",views.create_admin,name="seed_database")
     path("content_detail/",views.Admin_content_detail,name="content_detail")
+    path("content_search/",views.content_search,name="content_search")
 ]
